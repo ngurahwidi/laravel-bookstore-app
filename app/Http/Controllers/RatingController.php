e3f29a2ff81f8d5c5df97e3a21e5663a6041dbaa
ngurahwidi <widi050917@gmail.com>
2025-08-14 07:01:19 +0800

    fix seeder & add soft delete

--- app/Http/Controllers/RatingController.php
+++ app/Http/Controllers/RatingController.php
@@ -4,20 +4,10 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreRatingRequest;
 use App\Models\Author;
-use App\Models\Book;
-use App\Models\Rating;
 use App\Service\RatingService;
-use Illuminate\Http\Request;
 
 class RatingController extends Controller
 {
-    protected $ratingService;
-
-    public function __construct(RatingService $ratingService)
-    {
-        $this->ratingService = $ratingService;
-    }
-
     public function create()
     {
         $authors = Author::orderBy('name')->get();
@@ -26,7 +16,8 @@ class RatingController extends Controller
 
     public function store(StoreRatingRequest $request)
     {
-        $this->ratingService->store($request);
+        $ratingService = new RatingService();
+        $ratingService->store($request);
         return redirect()->route('books.index')->with('success', 'Rating successfully saved');
     }
 }
--- database/migrations/2025_08_11_150854_create_books_table.php
+++ database/migrations/2025_08_11_150854_create_books_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -17,6 +16,7 @@ return new class extends Migration
             $table->foreignId('author_id')->constrained('authors')->onDelete('cascade');
             $table->foreignId('category_id')->constrained('categories')->onDelete('cascade');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
